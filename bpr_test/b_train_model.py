4d26c264311e98f262795858d5adc0dcec694021
quyuanhang <qu_yh@pku.edu.cn>
2017-08-22 11:40:20 +0800

    完善评价指标

--- bpr_test/b_train_model.py
+++ bpr_test/b_train_model.py
@@ -84,4 +84,40 @@ for k in range(1, 100, 5):
 
 plt.scatter(precision_list, recall_list)
 
+def evaluate(recommend_dict, lable_dict, train_dict, top=1000, mode='base'):
+    tp, fp, fn = 0, 0, 0
+    precision_recall_list = list()
+    for exp, job_rank_dict in recommend_dict.items():
+        if exp in set(lable_dict.keys()) & set(train_dict.keys()):
+            job_rank = sorted(job_rank_dict.items(),
+                              key=lambda x: x[1], reverse=True)
+            rec = [j_r[0] for j_r in job_rank if j_r[0] not in train_dict[exp]][:top]
+            rec_set = set(rec)
+            positive_set = set(lable_dict[exp].keys()) - set(train_dict[exp].keys())
+            tp += len(rec_set & positive_set)
+            fp += len(rec_set - positive_set)
+            fn += len(positive_set - rec_set)
+            if len(positive_set) > 0:
+                if mode == 'max':
+                    precision = 1 if rec_set & positive_set else 0
+                    recall = 1 if rec_set & positive_set else 0
+                else:
+                    precision = len(rec_set & positive_set) / len(rec_set)
+                    recall = len(rec_set & positive_set) / len(positive_set)
+                precision_recall_list.append([precision, recall])
+    if (mode == 'base') or (mode == 'max'):
+        df = pd.DataFrame(precision_recall_list, columns=[
+                          'precision', 'recall'])
+        return pd.DataFrame([df.mean(), df.std()], index=['mean', 'std'])
+    elif mode == 'sum':
+        return ('precision, recall \n %f, %f' % ((tp / (tp + fp)), (tp / (tp + fn))))
+
+precision_list, recall_list = [], []
+for k in range(1, 100, 5):
+    precision, recall = evaluate(prediction, test_dict, train_dict, top=k, mode='base').values[0]
+    precision_list.append(precision)
+    recall_list.append(recall)
+
+plt.scatter(precision_list, recall_list)
+
 bpr.test(testing_data)
--- bpr_test/test_plot.py
+++
@@ -1,4 +0,0 @@
-import matplotlib.pylab as plt
-
-plt.scatter(x=[1,2,3], y=[1,2,3])
-plt.savefig('pic.jpg')
