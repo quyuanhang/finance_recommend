683804d0c752315894b1c709ce7d401a76bed5a4
quyuanhang <qu_yh@pku.edu.cn>
2017-08-23 16:04:15 +0800

    后融合模型完成

--- bpr_test/c_bagging.py
+++ bpr_test/c_bagging.py
@@ -2,7 +2,10 @@
 # 內建库
 import sys
 import json
-# 第三方库
+import random
+# 第三方库\
+import sklearn
+import numpy as np
 import pandas as pd
 import matplotlib.pylab as plt
 
@@ -17,8 +20,8 @@ with open(view_pre_file, encoding='utf8') as file:
 with open(click_pre_file, encoding='utf8') as file:
 	click_dict = json.load(file)
 
+user_set, tag_set, pos_set = set(), set(), set()
 data = []
-
 view_train_frame = pd.read_csv(view_train_file).drop_duplicates()
 for i, row in view_train_frame.iterrows():
 	user, tag = row
@@ -29,7 +32,12 @@ for i, row in view_train_frame.iterrows():
 		data.append([s1, s2, 1])
 	except:
 		continue
+	user_set.add(user)
+	tag_set.add(tag)
+	pos_set.add((user, tag))
+view_frame = pd.DataFrame(data)
 
+data = []
 click_train_frame = pd.read_csv(click_train_file).drop_duplicates()
 for i, row in click_train_frame.iterrows():
 	user, tag = row
@@ -40,10 +48,36 @@ for i, row in click_train_frame.iterrows():
 		data.append([s1, s2, 2])
 	except:
 		continue
+	user_set.add(user)
+	tag_set.add(tag)
+	pos_set.add((user, tag))		
+click_frame = pd.DataFrame(data)
 
-frame = pd.DataFrame(data)
+frame = pd.concat([view_frame, click_frame])
 x = frame.values[:, 0]
 y = frame.values[:, 1]
 c = frame.values[:, 2]
 
-plt.scatter(x, y, c=c, s=1, alpha=0.5)
+p = len(pos_set) / (len(user_set) * len(tag_set))
+
+neg_list = list()
+for user in user_set:
+	for tag in tag_set:
+		if random.random() < p and (user, tag) not in pos_set:
+			s1 = view_dict[user][tag]
+			s2 = click_dict[user][tag]
+			neg_list.append([s1, s2, 0])
+neg_frame = pd.DataFrame(neg_list)
+pos_neg_frame = pd.concat([frame, neg_frame])
+x = pos_neg_frame.values[:, 0]
+y = pos_neg_frame.values[:, 1]
+c = pos_neg_frame.values[:, 2]
+
+linear_model = sklearn.linear_model
+reg = linear_model.LinearRegression()
+reg.fit(pos_neg_frame.values[:, :2], pos_neg_frame.values[:, 2])
+
+color_dict = {0:'gray', 1:'b', 2:'r'}
+color = [color_dict[i] for i in c]
+plt.scatter(x, y, c=color, s=2, alpha=0.4)
+print(reg)
