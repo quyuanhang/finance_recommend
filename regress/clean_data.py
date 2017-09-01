3e49d59ddb75c084dda94463e58dd8aedcbdf497
quyuanhang <qu_yh@pku.edu.cn>
2017-09-01 15:52:31 +0800

    更正了数据 得到了真实结果 行业字段待添加

--- regress/clean_data.py
+++ regress/clean_data.py
@@ -8,7 +8,9 @@ import numpy as np
 
 investor_file = 'data/investor.csv'
 company_file = 'data/company.csv'
-view_file = 'data/view_com_828.csv'
+view_file = 'data/view_com_820.csv'
+bp_file = 'data/bp_820.csv'
+wb_file = 'data/workbench_820.csv'
 
 def prefer_phases(frame):
     frame = frame.copy()
@@ -31,7 +33,7 @@ def prefer_phases(frame):
     frame['max_phase'] = max_list
     return frame.drop('prefer_phase', axis=1)
 
-def prefer_industry(frame):
+def prefer_industry(frame, key):
     def pivot(data):
         data_dict = dict()
         for row, line in enumerate(data):
@@ -45,17 +47,10 @@ def prefer_industry(frame):
                     data_dict[row][col] = 1
         df = pd.DataFrame(data_dict).fillna(0)
         return df.T
-    pre = pivot(frame['prefer_industry'].values)
+    pre = pivot(frame[key].values)
     pre.columns = map(lambda x: 'pref_' + str(x), list(pre.columns))
-    frame = pd.merge(frame, pre, left_index=True, right_index=True)
-    return frame.drop('prefer_industry', axis=1)
-
-investor_frame = pd.read_csv(investor_file)
-investor_frame = prefer_phases(investor_frame)
-investor_frame = prefer_industry(investor_frame)
-
-company_frame = pd.read_csv(company_file)
-view_frame = pd.read_csv(view_file)
+    frame = pd.merge(frame, pre, left_index=True, right_index=True, how='left')
+    return frame.drop(key, axis=1)
 
 def fast_sample(posi_frame, _len=1):
     user_array = posi_frame.iloc[:, 0].values
@@ -73,26 +68,63 @@ def fast_sample(posi_frame, _len=1):
     df_negative_sample['num'] = 0
     return df_negative_sample
 
-combine_frame = pd.merge(view_frame, investor_frame, left_on='id', right_on='user_id')
-combine_frame = pd.merge(combine_frame, company_frame, left_on='cid', right_on='attach_cid')
-combine_frame['address'] = [1 if i[0] == i[1] else 0 for i in zip(combine_frame['address1_x'], combine_frame['address1_y'])]
-combine_frame = combine_frame.fillna(0)
+class CidLib(object):
+    """docstring for id_converter"""
+    def __init__(self, id_cid_frame):
+        self.id_cid_dict = dict(zip(id_cid_frame['id'], id_cid_frame['attach_cid']))
 
-neg_frame = fast_sample(combine_frame[['id_x', 'cid']], 3)
+    def convert_id_to_cid(self, _id):
+        if _id in self.id_cid_dict:
+            cid = self.id_cid_dict[_id]
+        else:
+            cid = 0
+        return cid
+
+
+# 用户特征处理
+investor_frame = pd.read_csv(investor_file).drop_duplicates(subset='user_id')
+investor_frame = prefer_phases(investor_frame)
+investor_frame = prefer_industry(investor_frame, 'prefer_industry')
+investor_frame = investor_frame.fillna(0).astype('int')
+
+# 公司特征处理
+company_frame = pd.read_csv(company_file).drop_duplicates(subset='attach_cid')
+company_frame = prefer_industry(company_frame, 'industry1')
+company_frame = company_frame.fillna(0).astype('int')
+cid_lib = CidLib(company_frame.iloc[:, :2])
+
+# bp行为
+bp_frame = pd.read_csv(bp_file)
+bp_frame['cid'] = bp_frame['crm_id'].map(cid_lib.convert_id_to_cid)
+bp_frame['cid'] = bp_frame['company_id'].fillna(0) + bp_frame['cid']
+bp_frame.drop(['crm_id', 'company_id'], axis=1, inplace=True)
+bp_frame = bp_frame.reindex(columns=['id', 'cid', 'num'])
+
+# workbench行为
+wb_frame = pd.read_csv(wb_file)
+wb_frame['cid'] = wb_frame['ccid'].map(cid_lib.convert_id_to_cid)
+wb_frame.drop(['ccid'], axis=1, inplace=True)
+wb_frame = wb_frame.reindex(columns=['id', 'cid', 'num'])
+
+view_frame = pd.read_csv(view_file)
+
+posi_combine_frame = pd.merge(view_frame, investor_frame, left_on='id', right_on='user_id')
+posi_combine_frame = pd.merge(posi_combine_frame, company_frame, left_on='cid', right_on='attach_cid')
+posi_combine_frame['address'] = [1 if i[0] == i[1] else 0 for i in zip(posi_combine_frame['address1_x'], posi_combine_frame['address1_y'])]
+posi_combine_frame = posi_combine_frame.fillna(0)
+
+neg_frame = fast_sample(posi_combine_frame[['id_x', 'cid']], 1)
 neg_combine_frame = pd.merge(neg_frame, investor_frame, left_on='id', right_on='user_id')
 neg_combine_frame = pd.merge(neg_combine_frame, company_frame, left_on='cid', right_on='attach_cid')
 neg_combine_frame['address'] = [1 if i[0] == i[1] else 0 for i in zip(neg_combine_frame['address1_x'], neg_combine_frame['address1_y'])]
 neg_combine_frame = neg_combine_frame.fillna(0)
 
-combine_frame = pd.concat([combine_frame, neg_combine_frame])
+combine_frame = pd.concat([posi_combine_frame, neg_combine_frame])
 
 
 data = combine_frame.drop(['id_x', 'cid', 'num', 'id_y', 'user_id', 'org_id', 'id.1', 'org_id.1', 'address1_x', 'address1_y', 'id', 'attach_cid'], axis=1)
-data['y'] = combine_frame['num'].apply(lambda x: 2 if x > 1 else x).values
-# data_y = combine_frame['num'].apply(lambda x: 2 if x > 1 else x).values
-# data_y = combine_frame['num'].apply(lambda x: 1 if x > 1 else 0).values
-
-# np.savetxt('data/data.csv', np.c_[data_x, data_y], delimiter = ',')
+# data['y'] = combine_frame['num'].map(lambda x: 2 if x >= 2 else x).values
+data['y'] = combine_frame['num'].map(lambda x: 1 if x >= 1 else x).values
 
 data.to_csv('data/data.csv', index=False)
 
--- regress/data/user_profile.sql
+++ regress/data/user_profile.sql
@@ -1,132 +1,96 @@
 SELECT * from(
-	(SELECT * from	
-		(SELECT 
-			id, user_id, org_id as a_oid, position,
-			(case when intro is null then 0 ELSE 1 END) as intro,
-			(case when school is null then 0 ELSE 1 END) as school,
-			(case when single_invest_unit=1 or single_invest_unit=3 then 1 else 0 end) as rmb,
-			(case when single_invest_unit=2 or single_invest_unit=3 then 1 else 0 end) as usd
-			from investor2
-			where is_36kr=0
-			and is_deleted=0
-			and org_type=0) as a
-		inner join
-		(SELECT
-			id as b_oid, prefer_industry, prefer_phase, manage_capital,
-			(case when logo is null then 0 else 1 end) as logo,
-			(case when website is null then 0 else 1 end) as website,
-			(case when weixin is null then 0 else 1 end) as weixin,
-			(case when weibo is null then 0 else 1 end) as weibo,
-			(case when partner_background is null then 0 else 1 end) as partner_background,
-			(case when manage_capital_currency='RMB' then 1 else 0 end) as rmb,
-			(case when manage_capital_currency='USD' then 1 else 0 end) as usd,
-			(case when capital_type='本土' then 1 else 0 end) as native,
-			(case when capital_type='外资' then 1 else 0 end) as foreign_,
-			(case when capital_type='合资' then 1 else 0 end) as joint,
-			(case when org_type=1 or org_type=2 then 1 else 0 end) as org_is_angle,
-			(case when org_type=3 or org_type=5 then 1 else 0 end) as org_is_vc,
-			(case when org_type=4 or org_type=5 then 1 else 0 end) as org_is_pe,
-			(case when org_type=6 then 1 else 0 end) as org_is_fofs,
-			(case when org_type=7 then 1 else 0 end) as org_is_strategic,
-			(case when org_type=9 then 1 else 0 end) as org_is_bank,
-			(case when org_type=10 then 1 else 0 end) as org_is_fund,
-			(case when org_type=11 then 1 else 0 end) as org_is_policy,
-			(case when org_type=12 then 1 else 0 end) as org_is_incubator,
-			(case when org_type=13 then 1 else 0 end) as org_is_fa
-			from organization2) as b
-		on a.a_oid = b.b_oid) as c
-	inner join
-	(SELECT 
-		org_id as c_oid, address1 from organization_address) as c
-	on a.a_oid=b.b_oid=c.c_oid
+    (SELECT 
+        id, user_id, org_id, 
+        (case when position like '%合伙人' or position like '董事%' then 1 else 0 end) as position_partner,
+        (case when position like '%总裁' or position like '%总经理' then 1 else 0 end) as position_manager,
+        (case when position like '%投资%' then 1 else 0 end) as position_inves,
+        (case when position like '%分析师' then 1 else 0 end) as position_fa,
+        (case when position like '秘书长' or position like '合伙人助理' then 1 else 0 end) as position_assistent,
+        (case when intro is null then 0 ELSE 1 END) as intro,
+        (case when school is null then 0 ELSE 1 END) as school,
+        (case when single_invest_unit=1 or single_invest_unit=3 then 1 else 0 end) as rmb,
+        (case when single_invest_unit=2 or single_invest_unit=3 then 1 else 0 end) as usd
+        from investor2
+        where is_36kr=0
+        and is_deleted=0
+        and org_type=0) as a 
+    inner join
+    (SELECT
+        id, prefer_industry, prefer_phase, manage_capital,
+        (round(DATEDIFF(current_date(),start_date)/30)) as age_month, 
+        (case when logo is null then 0 else 1 end) as logo,
+        (case when website is null then 0 else 1 end) as website,
+        (case when weixin is null then 0 else 1 end) as weixin,
+        (case when weibo is null then 0 else 1 end) as weibo,
+        (case when partner_background is null then 0 else 1 end) as partner_background,
+        (case when manage_capital_currency='RMB' then 1 else 0 end) as org_rmb,
+        (case when manage_capital_currency='USD' then 1 else 0 end) as org_usd,
+        (case when capital_type='本土' then 1 else 0 end) as native,
+        (case when capital_type='外资' then 1 else 0 end) as foreig,
+        (case when capital_type='合资' then 1 else 0 end) as joint,
+        (case when org_type=1 or org_type=2 then 1 else 0 end) as org_is_angle,
+        (case when org_type=3 or org_type=5 then 1 else 0 end) as org_is_vc,
+        (case when org_type=4 or org_type=5 then 1 else 0 end) as org_is_pe,
+        (case when org_type=6 then 1 else 0 end) as org_is_fofs,
+        (case when org_type=7 then 1 else 0 end) as org_is_strategic,
+        (case when org_type=9 then 1 else 0 end) as org_is_bank,
+        (case when org_type=10 then 1 else 0 end) as org_is_fund,
+        (case when org_type=11 then 1 else 0 end) as org_is_policy,
+        (case when org_type=12 then 1 else 0 end) as org_is_incubator,
+        (case when org_type=13 then 1 else 0 end) as org_is_fa
+        from organization2) as b on a.org_id = b.id
+    left join
+    (SELECT org_id, address1 from organization_address) as c on a.org_id = c.org_id)
+    
 
-SELECT * from(
-	(SELECT 
-		id, user_id, org_id, 
-		(case when position like '%合伙人' or position like '董事%' then 1 else 0 end) as position_partner,
-		(case when position like '%总裁' or position like '%总经理' then 1 else 0 end) as position_manager,
-		(case when position like '%投资%' then 1 else 0 end) as position_inves,
-		(case when position like '%分析师' then 1 else 0 end) as position_fa,
-		(case when position like '秘书长' or position like '合伙人助理' then 1 else 0 end) as position_assistent,
-		(case when intro is null then 0 ELSE 1 END) as intro,
-		(case when school is null then 0 ELSE 1 END) as school,
-		(case when single_invest_unit=1 or single_invest_unit=3 then 1 else 0 end) as rmb,
-		(case when single_invest_unit=2 or single_invest_unit=3 then 1 else 0 end) as usd
-		from investor2
-		where is_36kr=0
-		and is_deleted=0
-		and org_type=0) as a 
-	inner join
-	(SELECT
-		id, prefer_industry, prefer_phase, manage_capital,
-		(round(DATEDIFF(current_date(),start_date)/30)) as age_month, 
-		(case when logo is null then 0 else 1 end) as logo,
-		(case when website is null then 0 else 1 end) as website,
-		(case when weixin is null then 0 else 1 end) as weixin,
-		(case when weibo is null then 0 else 1 end) as weibo,
-		(case when partner_background is null then 0 else 1 end) as partner_background,
-		(case when manage_capital_currency='RMB' then 1 else 0 end) as org_rmb,
-		(case when manage_capital_currency='USD' then 1 else 0 end) as org_usd,
-		(case when capital_type='本土' then 1 else 0 end) as native,
-		(case when capital_type='外资' then 1 else 0 end) as foreig,
-		(case when capital_type='合资' then 1 else 0 end) as joint,
-		(case when org_type=1 or org_type=2 then 1 else 0 end) as org_is_angle,
-		(case when org_type=3 or org_type=5 then 1 else 0 end) as org_is_vc,
-		(case when org_type=4 or org_type=5 then 1 else 0 end) as org_is_pe,
-		(case when org_type=6 then 1 else 0 end) as org_is_fofs,
-		(case when org_type=7 then 1 else 0 end) as org_is_strategic,
-		(case when org_type=9 then 1 else 0 end) as org_is_bank,
-		(case when org_type=10 then 1 else 0 end) as org_is_fund,
-		(case when org_type=11 then 1 else 0 end) as org_is_policy,
-		(case when org_type=12 then 1 else 0 end) as org_is_incubator,
-		(case when org_type=13 then 1 else 0 end) as org_is_fa
-		from organization2) as b on a.org_id = b.id
-	inner join
-	(SELECT org_id, address1 from organization_address) as c on a.org_id = c.org_id)
-	
-
-SELECT
-	id, attach_cid, industry1, address1, finance_phase,
-	(round(DATEDIFF(current_date(),start_date)/30)) as age_month, 
-	(case when logo is null then 0 else 1 end) as logo,
-	(case when website is null then 0 else 1 end) as website,
-	(case when scale='少于15人' then 1
-				when scale='15-50人' then 2
-				when scale='50-150人' then 3
-				when scale='150-500人' then 4
-				when scale='2000人以上' then 5
-				else 0 end) as scale,
-	(case when operation_status=-1 then 1 else 0 end) as pre_op,
-	(case when operation_status=0 then 1 else 0 end) as op,
-	(case when android_link is not null then 1 else 0 end) as android,
-	(case when iphone_appstore_link is not null then 1 else 0 end) as ios,
-	(case when address is not null then 1 else 0 end) as address,
-	(case when telephone is not null and telephone not like '1%' then 1 else 0 end) as telephone,
-	(case when telephone is not null and telephone like '1%' then 1 else 0 end) as mobile_phone,
-	(case when email is not null then 1 else 0 end) as email,
-	(case when weixin is null then 0 else 1 end) as weixin,
-	(case when weibo is null then 0 else 1 end) as weibo,
-	(case when file_business_plan is null then 0 else 1 end) as bp,
-	(case when itjuzi_id is null then 0 else 1 end) as itjuzi_id,
-	(case when lagou_id is null then 0 else 1 end) as lagou_id,
-	(case when xiniu_id is null then 0 else 1 end) as xiniu_id,
-	(case when product_and_service is null then 0 else 1 end) as product_and_service,
-	(case when market_and_customer is null then 0 else 1 end) as market_and_customer,
-	(case when business_model is null then 0 else 1 end) as business_model,
-	(case when operation_data is null then 0 else 1 end) as operation_data,
-	(case when core_resource is null then 0 else 1 end) as core_resource
+(SELECT
+    id, attach_cid, industry1, address1, finance_phase,
+    (round(DATEDIFF(current_date(),start_date)/30)) as age_month, 
+    (case when logo is null then 0 else 1 end) as logo,
+    (case when website is null then 0 else 1 end) as website,
+    (case when scale='少于15人' then 1
+                when scale='15-50人' then 2
+                when scale='50-150人' then 3
+                when scale='150-500人' then 4
+                when scale='2000人以上' then 5
+                else 0 end) as scale,
+    (case when operation_status=-1 then 1 else 0 end) as pre_op,
+    (case when operation_status=0 then 1 else 0 end) as op,
+    (case when android_link is not null then 1 else 0 end) as android,
+    (case when iphone_appstore_link is not null then 1 else 0 end) as ios,
+    (case when address is not null then 1 else 0 end) as address,
+    (case when telephone is not null and telephone not like '1%' then 1 else 0 end) as telephone,
+    (case when telephone is not null and telephone like '1%' then 1 else 0 end) as mobile_phone,
+    (case when email is not null then 1 else 0 end) as email,
+    (case when weixin is null then 0 else 1 end) as weixin,
+    (case when weibo is null then 0 else 1 end) as weibo,
+    (case when file_business_plan is null then 0 else 1 end) as bp,
+    (case when itjuzi_id is null then 0 else 1 end) as itjuzi_id,
+    (case when lagou_id is null then 0 else 1 end) as lagou_id,
+    (case when xiniu_id is null then 0 else 1 end) as xiniu_id,
+    (case when product_and_service is null then 0 else 1 end) as product_and_service,
+    (case when market_and_customer is null then 0 else 1 end) as market_and_customer,
+    (case when business_model is null then 0 else 1 end) as business_model,
+    (case when operation_data is null then 0 else 1 end) as operation_data,
+    (case when core_resource is null then 0 else 1 end) as core_resource
 FROM
 company
 WHERE operation_status in (-1, 0)
 and finance_phase >= 0
-and status = 2
+and status = 2)
 
 
 select count(*) from
-	(
-		(select id, cid, count(*) as num from (select distinct_id as id, company_id as cid from events as e join users as u on e.user_id = u.id where e.event = 'ViewCom' and e.date >= date'2017-8-20' and u.is_36kr is null and u.investor = 1) as tmp group by id, cid) as a
-		inner join 
-		(select user_id, org_id from mysql_online.crm.investor2 where is_36kr=0 and is_deleted=0 and org_type=0) as b on a.id = cast( b.user_id as varchar(20))
-		inner join 
-		(select attach_cid as id from mysql_online.crm.company where operation_status in (-1, 0) and finance_phase >= 0) as c on a.cid = c.id
-	) as d
+  (
+    (select id, cid, count(*) as num from (select distinct_id as id, company_id as cid from events as e join users as u on e.user_id = u.id where e.event = 'ViewCom' and e.date >= date'2017-8-20' and u.is_36kr is null and u.investor = 1) as tmp group by id, cid) as a
+    inner join 
+    (SELECT * from(
+        (SELECT id, user_id, org_id from investor2 where is_36kr=0 and is_deleted=0 and org_type=0) as a 
+        inner join
+        (SELECT id, prefer_industry, prefer_phase, manage_capital from organization2) as b on a.org_id = b.id)
+    ) as b on a.id = cast(b.user_id as varchar(20))
+    inner join 
+    ((SELECT id, attach_cid, industry1, address1, finance_phase FROM company WHERE operation_status in (-1, 0) and finance_phase >= 0 and status = 2)) 
+    as c on a.cid = c.id
+  ) as d
+
